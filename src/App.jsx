fd6c3bc2233e0f3b367378ac3e15bccfbc10e962
statesjs <statesjs@gmail.com>
2025-05-05 08:23:33 -0700

    xtra cred

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,27 @@ import "./App.css";
 import MovieDisplay from "./components/MovieDisplay";
 import Form from "./components/Form";
 
+//list of random movies i like
+const movies = [
+  "The Matrix",
+  "Inception",
+  "Up",
+  "Slumdog Millionaire",
+  "The Godfather",
+  "The Pursuit of Happyness",
+  "Spirited Away",
+  "The Dark Knight",
+  "rumble in the bronx",
+  "Forrest Gump",
+  "a beautiful mind",
+];
+
+//helper random index number
+const getRandomMovie = () => {
+  const randomIndex = Math.floor(Math.random() * movies.length);
+  return movies[randomIndex];
+};
+
 export default function App() {
   const apiKey = "55b92172";
 
@@ -21,7 +42,8 @@ export default function App() {
   };
 
   useEffect(() => {
-    getMovie("Clueless");
+    const randomTitle = getRandomMovie();
+    getMovie(randomTitle);
   }, []);
 
   return (
