5502cb72426a4f9e25d61231dad7cb021b079224
statesjs <statesjs@gmail.com>
2025-05-05 08:11:01 -0700

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,19 @@
 import { useState, useEffect } from "react";
-
 import "./App.css";
-
 import MovieDisplay from "./components/MovieDisplay";
 import Form from "./components/Form";
 
 export default function App() {
-  // Variable with your API Key
   const apiKey = "55b92172";
 
-  // State to hold movie data
   const [movie, setMovie] = useState(null);
 
-  // Function to get movies
   const getMovie = async (searchTerm) => {
-    // Make fetch request and store the response
     try {
       const response = await fetch(
         `http://www.omdbapi.com/?apikey=${apiKey}&t=${searchTerm}`
       );
-      // Parse JSON response into a JavaScript object
       const data = await response.json();
-      // Set the Movie state to the received data
       setMovie(data);
     } catch (err) {
       console.error(err);
@@ -30,12 +22,11 @@ export default function App() {
 
   useEffect(() => {
     getMovie("Clueless");
-  });
+  }, []);
 
-  // We pass the getMovie function as a prop called moviesearch
-  // We pass movie as props to movie display
   return (
     <div className="App">
+      <h1>Movie Search App</h1>
       <Form moviesearch={getMovie} />
       <MovieDisplay movie={movie} />
     </div>
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,21 +1,35 @@
-export default function MovieDisplay({ movie }) {
-  // Function to return loaded JSX
-  const loaded = () => {
-    return (
-      <>
-        <h1>{movie.Title}</h1>
-        <h2>{movie.Genre}</h2>
-        <img src={movie.Poster} alt={movie.Title} />
-        <h2>{movie.Year}</h2>
-      </>
-    );
+import { useState, useEffect } from "react";
+
+export default function Form(props) {
+  // State to hold the data of our form
+  const [formData, setFormData] = useState({
+    searchterm: "",
+  });
+
+  // handleChange - updates formData when we type into form
+  const handleChange = (event) => {
+    // Use the event object to detect key, and value to update
+    setFormData({ ...formData, [event.target.name]: event.target.value });
   };
 
-  // Function to return loading JSX
-  const loading = () => {
-    return <h1>No Movie to Display</h1>;
+  const handleSubmit = (event) => {
+    // Prevent page from refreshing on form submission
+    event.preventDefault();
+    // Pass the search term to moviesearch prop, which is App's getMovie function
+    props.moviesearch(formData.searchterm);
   };
 
-  // Ternary operator will determine which functions JSX we will return
-  return movie ? loaded() : loading();
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="searchterm"
+          onChange={handleChange}
+          value={formData.searchterm}
+        />
+        <input type="submit" value="submit" />
+      </form>
+    </div>
+  );
 }
